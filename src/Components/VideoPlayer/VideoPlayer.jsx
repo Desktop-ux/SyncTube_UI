5a03a6c2710979f9643209915bccdef5ebe8ddd7
Ayaan <desktop.ux@gmail.com>
2026-03-17 00:50:27 +0530

    video player componenet created

--- src/Components/VideoPlayer/VideoPlayer.jsx
+++ src/Components/VideoPlayer/VideoPlayer.jsx
@@ -1,9 +1,71 @@
-import React from 'react'
-import './VideoPlayer.css'
+import { useEffect, useRef, useState } from "react"
+import YouTube from "react-youtube"
+import socket from "../../socket"
+import "./VideoPlayer.css"
 
-const VideoPlayer = () => {
+function VideoPlayer({ roomId }) {
+
+  const playerRef = useRef(null)
+  const [videoId, setVideoId] = useState("dQw4w9WgXcQ")
+
+  const opts = {
+    width: "800",
+    height: "450",
+    playerVars: {
+      autoplay: 0
+    }
+  }
+
+  const onReady = (event) => {
+    playerRef.current = event.target
+  }
+
+  const onPlay = () => {
+    socket.emit("play", { roomId })
+  }
+
+  const onPause = () => {
+    socket.emit("pause", { roomId })
+  }
+
+  const onSeek = () => {
+    const time = playerRef.current.getCurrentTime()
+    socket.emit("seek", { roomId, time })
+  }
+
+  useEffect(() => {
+
+    socket.on("play", () => {
+      playerRef.current.playVideo()
+    })
+
+    socket.on("pause", () => {
+      playerRef.current.pauseVideo()
+    })
+
+    socket.on("seek", ({ time }) => {
+      playerRef.current.seekTo(time)
+    })
+
+    socket.on("change_video", ({ videoId }) => {
+      setVideoId(videoId)
+    })
+
+  }, [])
+
   return (
-    <div>VideoPlayer</div>
+    <div className="video-container">
+
+      <YouTube
+        videoId={videoId}
+        opts={opts}
+        onReady={onReady}
+        onPlay={onPlay}
+        onPause={onPause}
+        onStateChange={onSeek}
+      />
+
+    </div>
   )
 }
 
--- src/Pages/Room/Room.jsx
+++ src/Pages/Room/Room.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
-import './Room.css'
+import { useEffect, useState } from "react"
+import socket from "../../socket"
+import VideoPlayer from "./../../Components/VideoPlayer/VideoPlayer"
 
-const Room = () => {
+function Room({ roomId, username }) {
+
+  const [participants, setParticipants] = useState([])
+
+  useEffect(() => {
+
+    socket.on("participants_update", (users) => {
+      setParticipants(users)
+    })
+
+  }, [])
+
   return (
-    <div>Room</div>
+    <div>
+
+      <h2>Room: {roomId}</h2>
+
+      <VideoPlayer roomId={roomId} />
+
+      <h3>Participants</h3>
+
+      <ul>
+        {participants.map((p) => (
+          <li key={p.id}>
+            {p.username} ({p.role})
+          </li>
+        ))}
+      </ul>
+
+    </div>
   )
 }
 
